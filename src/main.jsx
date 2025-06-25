b4b1b79316925d5e5238aa0033dd505bbeff0a17
Moon Awan <malikmoon.developer061@gmail.com>
2025-06-25 19:36:44 +0500

    Update React-route-dom

--- src/main.jsx
+++ src/main.jsx
@@ -8,9 +8,7 @@ import "react-toastify/dist/ReactToastify.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter
-      future={{ v7_startTransition: true, v7_relativeSplatPath: true }}
-    >
+    <BrowserRouter>
       <AuthProvider>
         <App />
         <ToastContainer draggable pauseOnFocusLoss={false} />
