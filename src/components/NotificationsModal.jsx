d97923b58530fbf9aaf646f464294af983344bfa
Moon Awan <malikmoon.developer061@gmail.com>
2025-06-27 20:13:29 +0500

    Update Code

--- src/components/NotificationsModal.jsx
+++ src/components/NotificationsModal.jsx
@@ -1,10 +1,25 @@
 import React, { useState } from "react";
 import moment from "moment";
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp, EllipsisVertical } from "lucide-react";
 
 const NotificationsModal = ({ onClose, selectedNotifications }) => {
   const [isAboutUser, setIsAboutUser] = useState(false);
 
+
+  const calculateAge = (dobString) => {
+    const dob = new Date(dobString);
+    const today = new Date();
+
+    let age = today.getFullYear() - dob.getFullYear();
+    const hasBirthdayPassed =
+      today.getMonth() > dob.getMonth() ||
+      (today.getMonth() === dob.getMonth() && today.getDate() >= dob.getDate());
+
+    if (!hasBirthdayPassed) age -= 1;
+
+    return age;
+  };
+  
   return (
     <div className="modal-overlay">
       <div className="modal-card" style={{ maxWidth: "410px" }}>
@@ -31,45 +46,73 @@ const NotificationsModal = ({ onClose, selectedNotifications }) => {
               <p>{selectedNotifications.userEmail}</p>
             </div>
           </div>
-          <div
-            style={{ marginTop: "20px" }}
-            className="report-user-container report-description-container"
-          >
-            <p>{selectedNotifications.description}</p>
-          </div>
-          <div className="report-user-container">
-            <button
-              onClick={() => {
-                setIsAboutUser((prev) => !prev);
-              }}
-              className="report-user-button"
+          {selectedNotifications.description && (
+            <div
+              style={{ marginTop: "20px" }}
+              className="report-user-container report-description-container"
             >
-              <p># Tagged Users</p>
-              <span>{isAboutUser ? <ChevronUp /> : <ChevronDown />}</span>
-            </button>
-            {isAboutUser && (
-              <div className="report-user-list">
-                <div className="devices-list">
-                  {selectedNotifications.reportAbout.map((report, index) => (
-                    <div
-                      className="device-item"
-                      style={{ boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 20px" }}
-                      key={index}
-                    >
-                      <strong>{index + 1}.</strong>
-                      <img src={report.userImg} />
-                      <div className="tag-users-detail" style={{ overflow: "hidden" }}>
-                        <h3>{report.name}</h3>
-                        <p>
-                          {report.email}
-                        </p>
+              <p>{selectedNotifications.description}</p>
+            </div>
+          )}
+          {selectedNotifications.userAge && (
+            <div
+              style={{ alignItems: "start" }}
+              className="report-user-container"
+            >
+              <p>
+                <strong>Age:</strong>{" "}
+                {selectedNotifications.userAge
+                  ? calculateAge(selectedNotifications.userAge) + " years"
+                  : "N/A"}
+              </p>
+            </div>
+          )}
+          {selectedNotifications.status && (
+            <div
+              style={{ alignItems: "start" }}
+              className="report-user-container"
+            >
+              <p>
+                <strong>Status:</strong> {selectedNotifications.status}
+              </p>
+            </div>
+          )}
+          {selectedNotifications.reportAbout?.length > 0 && (
+            <div className="report-user-container">
+              <button
+                onClick={() => {
+                  setIsAboutUser((prev) => !prev);
+                }}
+                className="report-user-button"
+              >
+                <p># Tagged Users</p>
+                <span>{isAboutUser ? <ChevronUp /> : <ChevronDown />}</span>
+              </button>
+              {isAboutUser && (
+                <div className="report-user-list">
+                  <div className="devices-list">
+                    {selectedNotifications.reportAbout.map((report, index) => (
+                      <div
+                        className="device-item"
+                        style={{ boxShadow: "rgba(0, 0, 0, 0.1) 0px 0px 20px" }}
+                        key={index}
+                      >
+                        <strong>{index + 1}.</strong>
+                        <img src={report.userImg} />
+                        <div
+                          className="tag-users-detail"
+                          style={{ overflow: "hidden" }}
+                        >
+                          <h3>{report.name}</h3>
+                          <p>{report.email}</p>
+                        </div>
                       </div>
-                    </div>
-                  ))}
+                    ))}
+                  </div>
                 </div>
-              </div>
-            )}
-          </div>
+              )}
+            </div>
+          )}
           <div
             style={{ alignItems: "start" }}
             className="report-user-container"
