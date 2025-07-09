015d66294392d461226f5fedba70611081880ee8
Moon Awan <malikmoon.developer061@gmail.com>
2025-07-09 20:56:32 +0500

    Update Vite build config for minification and targets

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,11 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   build: {
-    sourcemap: true,
+    sourcemap: true, 
+    minify: "esbuild",
+    target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
     optimizeDeps: {
-      include: ["@emotion/react", "@emotion/styled", "@mui/styled-engine"],
+      include: ["@emotion/react", "@emotion/styled", "@mui/styled-engine"], 
     },
   },
 });
