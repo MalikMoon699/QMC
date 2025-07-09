d25b6e6db4cb99c474ed037de167ea470af25b85
Moon Awan <malikmoon.developer061@gmail.com>
2025-07-09 21:07:41 +0500

    Update error

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    sourcemap: true,
+    minify: false, 
+    target: ["es2020", "edge88", "firefox78", "chrome87", "safari14"],
+    optimizeDeps: {
+      include: ["@emotion/react", "@emotion/styled", "@mui/styled-engine"],
+    },
+  },
+});
